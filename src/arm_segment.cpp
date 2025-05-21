b9c174cf729cda44ff0141caf225d29a5cc1a9fd
NathanielHawron <100041819+NathanielHawron@users.noreply.github.com>
2025-05-21 19:59:17 -0400

    Updated submodules

--- src/arm_segment.cpp
+++ src/arm_segment.cpp
@@ -16,7 +16,7 @@ size{length,radius},torque{torque},mass{mass},previous{previous},next{nullptr},b
         this->begin.changeParent(&this->previous->end);
     }
     this->end.changeParent(&this->begin);
-    this->end.moveA({0,this->size[0],0});
+    //this->end.moveA({0,this->size[0],0});
     
     this->renderable.init();
     this->renderable.setVBOLayout(ArmSegment::layout);
@@ -26,7 +26,7 @@ size{length,radius},torque{torque},mass{mass},previous{previous},next{nullptr},b
         20,
         NRA::VGL::MeshBuilder<GLuint>::Parameters::NORMAL_BIT,
         0, 0, offsetof(vertex, norm),
-        {0,0,this->size[0]/2}
+        {0,0,0}
     };
     NRA::VGL::Mesh<GLuint> mesh{sizeof(vertex)/4};
 
@@ -43,20 +43,20 @@ void ArmSegment::render(NRA::VGL::Shader &shader, std::string modelMatName){
 }
 void ArmSegment::renderUI(std::string label){
     std::string l = "Length / Radius " + label;
-    if(ImGui::SliderFloat2(l.c_str(),this->size,0.001f,100.0f,"%.3f")){
+    if(ImGui::SliderFloat2(l.c_str(),this->size,0.01f,10.0f,"%.2f")){
         NRA::VGL::MeshBuilder<GLuint>::Parameters p = {
             20,
             NRA::VGL::MeshBuilder<GLuint>::Parameters::NORMAL_BIT,
             0, 0, offsetof(vertex, norm),
-            {0,0,this->size[0]/2}
+            {0,0,0}
         };
         NRA::VGL::Mesh<GLuint> mesh{sizeof(vertex)/4};
         
         NRA::VGL::MeshBuilder<GLuint>::createCylinder(mesh, p, this->size[1], this->size[0]);
         
         this->renderable.loadMesh(mesh);
-        this->end.setZero();
-        this->end.moveA({0,this->size[0],0});
+        //this->end.setZero();
+        //this->end.moveA({0,this->size[0],0});
     }
 }
 void ArmSegment::renderUIR(std::size_t index){
